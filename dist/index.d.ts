a00ecbbc986e37683f4631803725fb9b47d4fbf0
Rodrigo Araujo <dygufa@gmail.com>
2018-02-01 17:31:57 -0300

    fixing dependencies

---
+++ dist/index.d.ts
@@ -0,0 +1,23 @@
+/// <reference types="react" />
+import * as React from "react";
+export interface IReactTextToInputProps {
+    value: string;
+    onChange: (value: string) => void;
+    textClassName?: string;
+    inputClassName?: string;
+    textStyle?: React.CSSProperties;
+    inputStyle?: React.CSSProperties;
+}
+export interface IReactTextToInputState {
+    editing: boolean;
+    value: string;
+}
+export default class ReactTextToInput extends React.Component<IReactTextToInputProps, IReactTextToInputState> {
+    constructor(props: IReactTextToInputProps);
+    componentWillReceiveProps(nextProps: IReactTextToInputProps): void;
+    startEditing: () => void;
+    onInputKeyPress: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+    onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onInputBlur: () => void;
+    render(): JSX.Element;
+}
---
+++ src/index.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+
+export interface IReactTextToInputProps {
+    value: string;
+    onChange: (value: string) => void;
+    textClassName?: string;
+    inputClassName?: string;
+    textStyle?: React.CSSProperties;
+    inputStyle?: React.CSSProperties;
+}
+
+export interface IReactTextToInputState {
+    editing: boolean;
+    value: string;
+}
+
+export default class ReactTextToInput extends React.Component<IReactTextToInputProps, IReactTextToInputState> {
+    constructor(props: IReactTextToInputProps) {
+        super(props);
+
+        this.state = {
+            editing: false,
+            value: props.value,
+        };
+    }
+
+    componentWillReceiveProps(nextProps: IReactTextToInputProps) {
+        this.setState({
+            value: nextProps.value,
+        });
+    }
+
+    startEditing = () => {
+        this.setState({
+            editing: true,
+        });
+    }
+
+    onInputKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            this.setState({
+                editing: false,
+            });
+
+            this.props.onChange(this.state.value);
+        }
+    }
+
+    onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+
+        this.setState({
+            value,
+        });
+    }
+
+    onInputBlur = () => {
+        this.setState({
+            editing: false,
+        });
+
+        this.props.onChange(this.state.value);
+    }
+
+    render() {
+        if (this.state.editing) {
+            return <input
+                autoFocus
+                value={this.state.value}
+                onChange={this.onInputChange}
+                onKeyPress={this.onInputKeyPress}
+                onBlur={this.onInputBlur}
+            />;
+        }
+
+        return <div
+            className={this.props.textClassName}
+            style={this.props.textStyle}
+            onClick={this.startEditing}
+        >
+            {this.props.value}
+        </div>;
+    }
+}
