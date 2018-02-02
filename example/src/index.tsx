6886848974142832bce4c23aa0aa71be022d241a
Rodrigo Araujo <dygufa@gmail.com>
2018-02-02 14:45:54 -0300

    adding example

---
+++ example/src/index.tsx
@@ -0,0 +1,39 @@
+import { render } from "react-dom";
+import * as React from "react";
+import TextToInput from "../../dist";
+
+interface IAppProps {
+
+}
+
+interface IAppState {
+    value: string
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+    state = {
+        value: "Clique em mim para editar"
+    }
+
+    onInputChange = (value: string) => {
+        this.setState({
+            value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Exemplo:</h1>
+
+                <TextToInput
+                    value={this.state.value}
+                    onChange={this.onInputChange}
+                />
+            </div>            
+        );
+    }
+}
+
+render(<App/>, document.getElementById("root")
+)
